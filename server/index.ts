91554bc6a582e1129db2728ea03e98679a229ee2
GeekTR <geektheripper@gmail.com>
2025-11-01 08:02:42 -0700

    use hono

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,9 @@
-export default {
-	fetch(request) {
-		const url = new URL(request.url);
+import { Hono } from "hono";
 
-		if (url.pathname.startsWith("/api/")) {
-			return Response.json({
-				name: "Cloudflare",
-			});
-		}
-		return new Response(null, { status: 404 });
-	},
-} satisfies ExportedHandler<Env>;
+const app = new Hono();
+
+app.get("/api/", (c) => {
+  return c.json({ name: "Cloudflare" });
+});
+
+export default app;
