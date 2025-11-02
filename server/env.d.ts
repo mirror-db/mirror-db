c618017e4f814f6724d96682d4c49cf837b9d672
GeekTR <geektheripper@gmail.com>
2025-11-01 21:44:12 -0700

    use d1

---
+++ server/env.d.ts
@@ -0,0 +1,3 @@
+export interface Bindings {
+    mirror_db: D1Database;
+}
--- server/index.ts
+++ server/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
+import type { Bindings } from "./env.d";
 
-const app = new Hono();
+const app = new Hono<{ Bindings: Bindings }>();
 
 app.get("/api/", (c) => {
   return c.json({ name: "Cloudflare" });
