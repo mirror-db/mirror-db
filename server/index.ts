3188c5d578f180833541e9680a12a96499c1f5fd
GeekTR <geektheripper@gmail.com>
2026-06-09 20:46:55 -0700

    Add .editorconfig, update package.json for hono dependency, and enhance server functionality

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,22 @@
-export default {
-	fetch(request) {
-		const url = new URL(request.url);
+import docker from "./oci/docker";
 
-		if (url.pathname.startsWith("/api/")) {
-			return Response.json({
-				name: "Cloudflare",
-			});
-		}
-		return new Response(null, { status: 404 });
-	},
+const notfound = () => new Response(null, { status: 404 });
+
+export default {
+  fetch(request, env, ctx) {
+    const host = request.headers.get("host");
+    if (!host) return notfound();
+    if (!host.endsWith(env.BASE_DOMAIN)) return notfound();
+    const subdomain = host.slice(0, -(env.BASE_DOMAIN.length + 1)) || "@";
+
+    if (subdomain === "dcr") return docker.fetch(request, env, ctx);
+
+    const url = new URL(request.url);
+    if (url.pathname.startsWith("/api/")) {
+      return Response.json({
+        name: subdomain,
+      });
+    }
+    return notfound();
+  },
 } satisfies ExportedHandler<Env>;
